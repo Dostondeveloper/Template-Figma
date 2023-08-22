32aee730a1f1c91663501ecf0bfb2aa948484a3c
behzodbaxtiyorov <behzodbaxtiyorov6@gmail.com>
2023-08-22 13:32:21 +0500

    header

---
+++ src/components/Header/Nav.jsx
@@ -0,0 +1,18 @@
+
+
+export default function Nav() {
+  return (
+    <div className="header__nav">
+        <div className="header__nav-logo">
+            <h1>LOGO</h1>
+        </div>
+        <div className="header__nav-list">
+            <ul className="header__nav-list_item">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
